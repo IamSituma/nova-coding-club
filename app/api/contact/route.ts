7b1503eddabbeb6de433e81defde6aee15d17e4d
IamSituma <situma@novageneration.tech>
2025-12-05 20:52:01 +0300

    added notificatios

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
 
-    const googleScriptUrl = "https://script.google.com/macros/s/AKfycbxgAxTugO_3yslah8fvVr29kyQYnownROntx1_TGl4gvoNRKLUd7nzLMPGHw0AR3KoH/exec";
+    const googleScriptUrl = "https://script.google.com/macros/s/AKfycbzExzmQar9Q7a1F8FT_ZCX9lkLcbpZe0svEz2JLropKsXdyaAoe6zAm9f2wZIDVsCOn/exec";
 
     const res = await fetch(googleScriptUrl, {
       method: "POST",
